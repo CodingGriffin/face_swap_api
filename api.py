5ccab78ff4acdae549c9a6137ea0d6b1467c7efc
KalidSittner <sittnerkalid@gmail.com>
2024-09-18 05:55:03 -0700

    middleware

--- api.py
+++ api.py
@@ -1,6 +1,22 @@
 from fastapi import FastAPI, UploadFile
-from face_swap import face_swap
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
+
 app = FastAPI()
+app.mount("/result_uploads", StaticFiles(directory="result_uploads"), name="static")
+origins = [
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+from face_swap import face_swap
 
 
 @app.post("/face-swap")
@@ -9,4 +25,4 @@ async def create_upload_file(avatar: UploadFile):
   with open(file_location, "wb+") as file_object:
       file_object.write(avatar.file.read())
   new_file_location = face_swap(file_location)
-  return {"path": new_file_location}
+  return {"result": new_file_location}
