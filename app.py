f4c88b9ed7728e87378aed206a0db09a7804f7fb
KalidSittner <sittnerkalid@gmail.com>
2024-09-21 04:11:56 -0700

    streaming

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 from fastapi import FastAPI, UploadFile
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
+from fastapi.responses import StreamingResponse
+from pathlib import Path
+import os
+import io
 
 app = FastAPI()
-app.mount("/result_uploads", StaticFiles(directory="result_uploads"), name="static")
 
 app.add_middleware(
     CORSMiddleware,
@@ -14,7 +17,7 @@ app.add_middleware(
 )
 
 from face_swap import face_swap
-
+IMAGES_DIR = Path("./result_uploads")
 
 @app.post("/face-swap")
 async def create_upload_file(avatar: UploadFile):
@@ -24,3 +27,12 @@ async def create_upload_file(avatar: UploadFile):
   new_file_location = face_swap(file_location)
   print(new_file_location)
   return {"result": new_file_location}
+
+@app.get("/result_uploads/{filename}")
+async def stream_image(filename: str):
+    file_path = IMAGES_DIR / filename
+    if file_path.exists() and file_path.is_file():
+        # Open the file and stream it
+        with open(file_path, "rb") as file:
+            return StreamingResponse(io.BytesIO(file.read()), media_type="image/jpeg")
+    return {"error": "File not found"}
